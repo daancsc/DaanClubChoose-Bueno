ff6a40900f121d86cae685327bf75ecec0413bd2
yoyo930021 <yoyo930021@gmail.com>
2017-08-23 11:55:26 +0800

    Fix a bug

--- client/src/components/Choose.vue
+++ client/src/components/Choose.vue
@@ -14,6 +14,7 @@
         <v-divider class="mb-4"></v-divider>
         <h4>2. 點選課程，使用按鈕移動志願序</h4>
         <p class="red--text">{{ (choose.length < 10)?"請填滿 10 個志願":"" }}</p>
+        <p class="red--text">{{ (!limit)?"必須填寫 跨群科體驗課程":"" }}</p>
         <v-layout class="ma-0" wrap row>
           <v-flex xs12 sm10>
             <ul>
@@ -26,6 +27,9 @@
             </v-btn>
             <v-btn class="blue" dark block @click.native="down">
               <v-icon>arrow_downward</v-icon>
+            </v-btn>
+            <v-btn class="blue" dark block @click.native="del">
+              <v-icon>delete</v-icon>
             </v-btn>
           </v-flex>
         </v-layout>
@@ -114,6 +118,16 @@ export default {
           result = true
         }
       }
+      return (result || !this.limit)
+    },
+    limit: function () {
+      var result = false
+      for (var i = 0; i < this.choose.length; i++) {
+        if (this.choose[i].id === 1) {
+          result = true
+          break
+        }
+      }
       return result
     },
     optitem: function () {
@@ -163,6 +177,18 @@ export default {
         var temp = this.choose[index + 1]
         this.$set(this.choose, index + 1, this.choose[index])
         this.$set(this.choose, index, temp)
+      }
+    },
+    del () {
+      var index = -1
+      for (var i = 0; i < this.choose.length; i++) {
+        if (this.choose[i].id === this.selectId) {
+          index = i
+          break
+        }
+      }
+      if (index !== -1) {
+        this.choose.splice(index, 1)
       }
     },
     submit () {
